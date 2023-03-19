0c4331c663e39b0a498320b7f89e2657706eccdb
Alexey <Lelik8888@gmail.com>
2023-03-19 13:56:52 +0300

    Add Permissions: -ACCESS_COARSE_LOCATION -ACCESS_FINE_LOCATION -INTERNET Add dependencies: -Volley -Picasso -Fragment -Location Add new kotlin class ViewModel

---
+++ app/src/main/java/com/example/myapplication/ViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ViewModel : ViewModel() {
+    val liveData = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<String>()
+
+}
